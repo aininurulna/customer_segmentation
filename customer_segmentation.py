76086b0a5c418a6f12dedbfbb33eb8f0ddcc9278
aininurulna <83540312+aininurulna@users.noreply.github.com>
2021-05-24 05:01:48 +0900

    Update customer_segmentation.py

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -1,36 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 #import the dataset
 data = spark.read.csv("dbfs:/FileStore/tables/joined_cust_sales-3.csv", inferSchema = True, header = True, sep = ",").cache()
-
-
-# In[ ]:
-
 
 #check the data type of each feature
 data.printSchema()
 
-
-# In[ ]:
-
-
 from pyspark.sql.functions import unix_timestamp, from_unixtime, to_date
 data = data.withColumn('new_purchase_date', to_date(unix_timestamp('purchase_date', 'yyyy/MM/dd').cast("timestamp"))).drop('purchase_date')
 
-
-# In[ ]:
-
-
 #sort by cust and purchase date
 data = data.orderBy(["cust", "new_purchase_date"], ascending=True)
-
-
-# In[ ]:
-
 
 #create flag of the i-th number of purchase (1st time, 2nd time, etc.) as a new feature
 from pyspark.sql.window import Window
@@ -40,38 +18,19 @@ from pyspark.sql.functions import unix_timestamp, from_unixtime, to_date
 window = Window.partitionBy(data['cust']).orderBy(data['new_purchase_date'])
 data = data.select('*', rank().over(window).alias('flag'))
 
-
-# In[ ]:
-
-
 #create a new feature, total spending a customer spends on the client
 resultsumtotal = data.groupBy('cust').sum('total_spending').select('cust','sum(total_spending)')
 
-
-# In[ ]:
-
-
 #create a new feature, the number of visit per customer
 from pyspark.sql import functions as F
-from pyspark.sql import Window
 w = Window.partitionBy('cust')
 data = data.withColumn('visit', F.max('flag').over(w)).where(F.col('flag') == F.col('visit')).drop('visit')
 
-
-# In[ ]:
-
-
-from pyspark.sql.functions import *
-
 data = data.alias('data')
 resultsumtotal = resultsumtotal.alias('resultsumtotal')
-
 data2 = resultsumtotal.join(data, resultsumtotal['cust'] == data['cust'], 'left').drop(resultsumtotal['cust'])
 
-
-# In[ ]:
-
-
+#transform the categorical variable
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer, OneHotEncoder
 stages = []
@@ -85,11 +44,7 @@ for cat_col in cat_cols:
 pipeline = Pipeline(stages=stages)
 data3 = pipeline.fit(data2).transform(data2)
 
-
-# In[ ]:
-
-
-# TODO: Build a better Regression pipeline
+#build a pipeline of vectorassembler and minmaxscaler
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.feature import MinMaxScaler
@@ -107,10 +62,6 @@ pipeline2 = Pipeline().setStages([
 ])
 
 dataready = pipeline2.fit(data3).transform(data3)
-
-
-# In[ ]:
-
 
 # Calculate cost and plot from the elbow method to define the optimum number of clusters
 import numpy as np
@@ -136,10 +87,6 @@ pl.ylabel('Score')
 pl.title('Elbow Curve')
 pl.show()
 
-
-# In[ ]:
-
-
 #according to the elbow method, our optimum number of clusters is 3
 from pyspark.ml.clustering import KMeans
 from pyspark.ml.evaluation import ClusteringEvaluator
@@ -149,9 +96,6 @@ predictions = model.transform(dataready)
 evaluator = ClusteringEvaluator()
 silhouette = evaluator.evaluate(predictions)
 print("Silhouette with squared euclidean distance = " + str(silhouette))
-
-
-# In[ ]:
 
 
 
